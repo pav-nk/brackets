da1360e62cc21e777159a24a99be51b30a86b211
pav_nk <pav_nk@vk.com>
2024-08-01 16:50:05 +0300

    feat: the function works with different brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const bracketsData = bracketsConfig.reduce((acc, item) =>{
+    const [openedBracket, closedBracket] = item;
+    acc[closedBracket] = openedBracket;
+    return acc;
+  }, {});
+  const openedBrackets = bracketsConfig.map((brackets) => brackets[0]);
+  const closedBrackets = bracketsConfig.map((brackets) => brackets[1]);
+  for (let bracket of str.split('')) {
+    if (openedBrackets.includes(bracket)) {
+      stack.push(bracket);
+    }
+    if (closedBrackets.includes(bracket)) {
+      if (stack[stack.length - 1] === bracketsData[bracket]) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  if (stack.length !== 0) {
+    return false;
+  }
+  return true;
 }
