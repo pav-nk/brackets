9af1fff764f10f2afc58b949af5e3a477f1898b8
pav_nk <pav_nk@vk.com>
2024-08-01 17:16:14 +0300

    feat: the functionality is implemented

--- src/index.js
+++ src/index.js
@@ -1,21 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
   const stack = [];
+  const exceptions = [];
   const bracketsData = bracketsConfig.reduce((acc, item) =>{
     const [openedBracket, closedBracket] = item;
+    if (openedBracket === closedBracket) {
+      exceptions.push(openedBracket);
+      return acc;
+    }
     acc[closedBracket] = openedBracket;
     return acc;
   }, {});
-  const openedBrackets = bracketsConfig.map((brackets) => brackets[0]);
-  const closedBrackets = bracketsConfig.map((brackets) => brackets[1]);
-  for (let bracket of str.split('')) {
-    if (openedBrackets.includes(bracket)) {
-      stack.push(bracket);
+  const openedBrackets = Object.values(bracketsData);
+  const closedBrackets = Object.keys(bracketsData);
+  for (let symbol of str.split('')) {
+    if (openedBrackets.includes(symbol)) {
+      stack.push(symbol);
     }
-    if (closedBrackets.includes(bracket)) {
-      if (stack[stack.length - 1] === bracketsData[bracket]) {
+    if (closedBrackets.includes(symbol)) {
+      if (stack[stack.length - 1] === bracketsData[symbol]) {
         stack.pop();
       } else {
         return false;
+      }
+    }
+    if (exceptions.includes(symbol)) {
+      if (stack[stack.length - 1] === symbol) {
+        stack.pop();
+      } else {
+        stack.push(symbol);
       }
     }
   }
